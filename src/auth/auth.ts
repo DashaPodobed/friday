08b2e2bd3a26a916729959a35edae097945f618c
DariaPodobed <dariamakarchik@gmail.com>
2021-06-30 23:59:07 +0300

    changed link

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: "https://neko-back.herokuapp.com/2.0/",
     withCredentials: true,
     headers: {
         'APY-KEY': '5a725941-ea89-49be-a495-54849d967e55'
