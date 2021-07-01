950c3c616f40c7a2baa92e3122ba145580909f72
DariaPodobed <dariamakarchik@gmail.com>
2021-07-01 12:13:05 +0300

    delete api-key

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,10 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
     baseURL: "https://neko-back.herokuapp.com/2.0/",
-    withCredentials: true,
-    headers: {
-        'APY-KEY': '5a725941-ea89-49be-a495-54849d967e55'
-    }
+    withCredentials: true
 })
 
 export const authAPI =  {
