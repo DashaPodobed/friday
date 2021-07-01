942143d4691dfbb68445add88e3dab82676ea63e
DariaPodobed <dariamakarchik@gmail.com>
2021-07-01 12:31:39 +0300

    changed a href

--- src/api/ForgorAPI.ts
+++ src/api/ForgorAPI.ts
@@ -11,7 +11,7 @@ export const forgotAPI = {
             email,
             from: "test-front-admin <ai73a@yandex.by>",
             message: `<div style="background-color: lime; padding: 15px">password recovery link: 
-                      <a href='http://localhost:3000/friday#/set_new_password/$token$'>link</a></div>`
+                      <a href='https://DashaPodobed.github.io/friday#/set_new_password/$token$'>link</a></div>`
         })
     }
 }
